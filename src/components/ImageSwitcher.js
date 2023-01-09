f41121b4927bd40f2293625aca602401f7258462
Nelson Melo <nelson.melo@beyondidentity.com>
2023-01-09 10:22:56 -0500

    Tweaks to imageswitcher

--- src/components/ImageSwitcher.js
+++ src/components/ImageSwitcher.js
@@ -5,8 +5,19 @@ const ImageSwitcher = ({lightSrc, darkSrc}) => {
   const {colorMode, setColorMode} = useColorMode();
   console.log(colorMode)
 
+  var imgSrc = "";
+  if (colorMode === 'dark') {
+      imgSrc = darkSrc;
+  } else if (colorMode === 'light') {
+      imgSrc = lightSrc;
+  } else {
+    console.log("color undefined");
+    console.log(colorMode);
+    imgSrc = lightSrc;
+  }
+
   return (
-    <img src={colorMode === 'dark'? darkSrc: lightSrc} />
+    <img src={imgSrc} />
   )
 }
 
