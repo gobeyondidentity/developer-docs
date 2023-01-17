13922afffa911a8f21a2a35ba2afd4c7c0b51737
Will May <4763133+Will5@users.noreply.github.com>
2023-01-17 13:08:52 -0500

    SDK Setup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,7 +90,7 @@ const config = {
         },
         remarkPlugins: [
           [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
-          [require('docusaurus-remark-plugin-tab-blocks'), {sync: true, labels: [['win', 'Windows'], ['mac', 'macOS'], ['android', 'Android'], ['ios', 'iOS'], ['javascript', 'JavaScript'], ['flutter', 'Flutter'], ['reactnative', 'React Native']]}]
+          [require('docusaurus-remark-plugin-tab-blocks'), {sync: true, labels: [['win', 'Windows'], ['mac', 'macOS']]}]
         ],
       },
     ],
@@ -103,7 +103,7 @@ const config = {
         sidebarPath: require.resolve('./sidebarsGuides.js'),
         remarkPlugins: [
           [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
-          [require('docusaurus-remark-plugin-tab-blocks'), {sync: true, labels: [['win', 'Windows'], ['mac', 'macOS'], ['android', 'Android'], ['ios', 'iOS'], ['javascript', 'JavaScript'], ['flutter', 'Flutter'], ['reactnative', 'React Native']]}]
+          [require('docusaurus-remark-plugin-tab-blocks'), {sync: true, labels: [['win', 'Windows'], ['mac', 'macOS']]}]
         ],
       },
     ],
