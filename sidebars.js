c42147929a28a139ac136c6eb26b51327e020d05
Will May <4763133+Will5@users.noreply.github.com>
2023-08-08 09:49:20 -0400

    Cleanup sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,18 +2,17 @@
 const sidebars = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
   mainSidebar: [
-
     {
       type: 'category',
-      label: 'Foundations',  
-      collapsed: false, 
-      collapsible: false,     
+      label: 'Foundations',
+      collapsed: false,
+      collapsible: false,
       link: {
         type: 'doc',
         id: 'foundations/foundations',
-      },  
-      items: [ 
-        'foundations/overview', 
+      },
+      items: [
+        'foundations/overview',
         'foundations/universal-passkeys',
         'foundations/platform-overview',
         'foundations/api-endpoints',
@@ -22,15 +21,15 @@ const sidebars = {
     {
       type: 'category',
       label: 'Getting Started',
-      collapsed: true, 
-      collapsible: false,  
+      collapsed: true,
+      collapsible: false,
       link: {
         type: 'doc',
         id: 'get-started/get-started',
-      },         
-      items: [ 
+      },
+      items: [
         'get-started/get-started-react',
-        'get-started/get-started-angular',  
+        'get-started/get-started-angular',
         'get-started/get-started-nextjs',
       ],
     },
@@ -38,33 +37,33 @@ const sidebars = {
     {
       type: 'category',
       label: 'Quickstarts',
-      collapsed: true, 
-      collapsible: false,  
+      collapsed: true,
+      collapsible: false,
       link: {
         type: 'doc',
         id: 'quickstarts/quickstart',
-      },         
-      items: [ 
-        
+      },
+      items: [
+
       ],
     },
 */
     {
       type: 'category',
       label: 'Authentication',
-      collapsed: true, 
-      collapsible: false,   
+      collapsed: true,
+      collapsible: false,
       link: {
         type: 'doc',
         id: 'authentication/authentication',
-      },     
-      items: [ 
+      },
+      items: [
         {
           type: 'category',
           label: 'Hosted Web',
-          collapsed: true, 
-          collapsible: true,      
-          items: [ 
+          collapsed: true,
+          collapsible: true,
+          items: [
             'authentication/hosted-add-authentication',
             'authentication/hosted-add-otp-email',
           ],
@@ -72,18 +71,18 @@ const sidebars = {
         {
           type: 'category',
           label: 'Embedded SDK',
-          collapsed: true, 
-          collapsible: true,      
-          items: [ 
-            'authentication/embedded-sdk-add-passkeys',    
-            'authentication/embedded-sdk-add-otp-email',   
+          collapsed: true,
+          collapsible: true,
+          items: [
+            'authentication/embedded-sdk-add-passkeys',
+            'authentication/embedded-sdk-add-otp-email',
 /*
             {
               type: 'category',
               label: 'Integrations',
-              collapsed: true, 
-              collapsible: true,           
-               items: [    
+              collapsed: true,
+              collapsible: true,
+               items: [
                 {
                   type: 'doc',
                   id: 'integration-guides/expo-react-native',
@@ -93,7 +92,7 @@ const sidebars = {
                   type: 'doc',
                   id: 'integration-guides/next-auth',
                   label: 'NextAuth',
-                }, 
+                },
              ],
             },
 */
@@ -113,13 +112,13 @@ const sidebars = {
     {
       type: 'category',
       label: 'How-to Guides',
-      collapsed: false, 
-      collapsible: false,   
+      collapsed: false,
+      collapsible: false,
       link: {
         type: 'doc',
         id: 'how-to/how-to-guides',
-      },      
-      items: [     
+      },
+      items: [
         'how-to/find-tenant-id',
         'how-to/create-realm',
         'how-to/add-resource-server',
@@ -132,9 +131,9 @@ const sidebars = {
         {
           type: 'category',
           label: 'Administrative',
-          collapsed: true, 
-          collapsible: true,      
-          items: [ 
+          collapsed: true,
+          collapsible: true,
+          items: [
             'how-to/invite-teammate',
             'how-to/login-another-account',
             'how-to/add-groups',
@@ -148,13 +147,13 @@ const sidebars = {
         {
           type: 'category',
           label: 'Access Tokens',
-          collapsed: true, 
-          collapsible: true, 
+          collapsed: true,
+          collapsible: true,
           link: {
             type: 'doc',
             id: 'api-tokens/api-tokens',
-          },      
-          items: [ 
+          },
+          items: [
             'api-tokens/api-token-overview',
             'api-tokens/api-scopes',
             'api-tokens/create-api-token',
@@ -164,19 +163,18 @@ const sidebars = {
             'api-tokens/enable-refresh-tokens',
           ],
         },
-
       ],
     },
     {
       type: 'category',
       label: 'Integration Guides',
-      collapsed: false, 
-      collapsible: false,     
+      collapsed: false,
+      collapsible: false,
       link: {
         type: 'doc',
         id: 'integration-guides/integration-guides',
       },
-      items: [ 
+      items: [
         {
           type: 'doc',
           id: 'integration-guides/bubble',
@@ -196,7 +194,7 @@ const sidebars = {
           type: 'doc',
           id: 'integration-guides/next-auth',
           label: 'NextAuth',
-        }, 
+        },
         {
           type: 'doc',
           id: 'integration-guides/wordpress',
@@ -205,9 +203,9 @@ const sidebars = {
         {
           type: 'category',
           label: 'Primary IDP Integrations',
-          collapsed: true, 
-          collapsible: true,        
-          items: [ 
+          collapsed: true,
+          collapsible: true,
+          items: [
             {
               type: 'doc',
               id: 'integration-guides/auth0',
@@ -253,7 +251,6 @@ const sidebars = {
               id: 'integration-guides/okta-oidc',
               label: 'Okta OIDC',
             },
-            
           ],
         },
       ],
@@ -261,26 +258,22 @@ const sidebars = {
     {
       type: 'category',
       label: 'Resources',
-      collapsed: false, 
-      collapsible: false, 
+      collapsed: false,
+      collapsible: false,
       link: {
         type: 'doc',
         id: 'resources',
-      }, 
+      },
       items: [
         'glossary',
         {
           type: 'link',
           label: 'Service Status',
           href: 'https://status.beyondidentity.com/',
-        },   
-      ]
+        },
+      ],
     },
-
-    
   ],
-
-
 
   sdkSidebar: [
     {
@@ -292,20 +285,20 @@ const sidebars = {
       type: 'html',
       value: '<hr>',
     },
-    'sdks/sdk-setup', 
-    'sdks/sdk-size'      ,
+    'sdks/sdk-setup',
+    'sdks/sdk-size',
     {
       type: 'category',
       label: 'JavaScript',
-      collapsed: true, 
-      collapsible: true,         
-      items: [ 
+      collapsed: true,
+      collapsible: true,
+      items: [
         {
           type: 'category',
           label: 'JS Reference guide',
-          collapsed: true, 
-          collapsible: true,      
-          items: [ 
+          collapsed: true,
+          collapsible: true,
+          items: [
             'sdks/js-reference-guide/js-reference-initialize',
             'sdks/js-reference-guide/js-reference-isBindPasskeyUrl',
             'sdks/js-reference-guide/js-reference-bindPasskey',
@@ -319,23 +312,23 @@ const sidebars = {
             'sdks/js-reference-guide/js-reference-redeemOtp',
           ],
         },
-       {
+        {
           type: 'link',
           label: 'Documentation',
           href: 'https://gobeyondidentity.github.io/bi-sdk-js/',
-        },       
+        },
       ],
     },
     {
       type: 'link',
       label: 'Kotlin',
       href: 'https://gobeyondidentity.github.io/bi-sdk-android/',
-    },  
+    },
     {
       type: 'link',
       label: 'Flutter',
       href: 'https://gobeyondidentity.github.io/bi-sdk-flutter/',
-    }, 
+    },
     {
       type: 'link',
       label: 'React Native',
@@ -345,10 +338,8 @@ const sidebars = {
       type: 'link',
       label: 'Swift',
       href: 'https://gobeyondidentity.github.io/bi-sdk-swift/documentation/beyondidentityembedded/',
-    }, 
+    },
   ],
-
-
 };
 
 module.exports = sidebars;
